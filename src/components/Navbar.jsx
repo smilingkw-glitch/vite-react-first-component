3f9e0ccb5191f29068c4833fb1da0e9f2fd10992
smilingkw-glitch <smiling.kw@gmail.com>
2025-12-16 19:02:55 +0530

    Add Navbar component with basic styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ backgroundColor: '#333', padding: '20px', color: 'white' }}>
+      <h3>This is Navbar</h3>
+    </nav>
+  );
+}
+
+export default Navbar;
