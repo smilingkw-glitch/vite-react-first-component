c2a0a98805e36fde09af3545f772a3334b8af22d
smilingkw-glitch <smiling.kw@gmail.com>
2025-12-16 19:08:14 +0530

    Create MainPage component with Navbar, Content, and Footer

---
+++ src/MainPage.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar'
+import Content from './components/Content'
+import Footer from './components/Footer'
+
+function MainPage() {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default MainPage;
