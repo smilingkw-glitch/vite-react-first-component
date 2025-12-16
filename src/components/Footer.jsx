e9750a8e0762765cea11d210aa46655e448a0db9
smilingkw-glitch <smiling.kw@gmail.com>
2025-12-16 19:06:51 +0530

    Add Footer component with styling

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#555', color: '#ccc', fontSize: '12px', padding: '10px', marginTop: '20px' }}>
+      <p>This is Footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
